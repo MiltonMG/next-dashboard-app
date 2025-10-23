8a9bb38fc051ed169e4b210a55f8a278e063ee8b
Milton MG <milton.elsalvador26@gmail.com>
2025-10-22 21:13:17 -0600

    Update header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,13 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         suppressHydrationWarning
         className={`${monserrat.className} antialiased p-2`}
       >
-        <header className="bg-blue-600 py-10 flex justify-center items-center text-white text-xl rounded-md antialiased">
+        {/* <header className="bg-blue-600 py-10 flex justify-center items-center text-white text-xl rounded-md antialiased">
           Este es el header de la pagina main!
-        </header>
+        </header> */}
         {children}
-        <footer className="bg-blue-600 py-10 flex justify-center items-center text-white text-xl rounded-md antialiased">
+        {/* <footer className="bg-blue-600 py-10 flex justify-center items-center text-white text-xl rounded-md antialiased">
           Hecho con amor por la gente de vercel!
-        </footer>
+        </footer> */}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`text-xl text-gray-800 md:text-4xl md:leading-normal`}>
             <strong className={`${lusitana.className}`}>
               Welcome to Acme!!.
             </strong>{" "}
